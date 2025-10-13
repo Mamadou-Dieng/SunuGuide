b190e1eff75b5d3b8daa92a653bee4d665c8a8e9
Mamadou DIENG <m.d389@zig.univ.sn>
2025-10-13 23:01:04 +0000

    Initial commit

---
+++ src/main/java/sunuguide/SunuGuideApplication.java
@@ -0,0 +1,13 @@
+package sunuguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunuGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunuGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sunuguide/SunuGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package sunuguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunuGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
