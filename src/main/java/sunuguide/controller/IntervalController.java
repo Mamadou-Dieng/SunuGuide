9d9218cbbf30a1f732251badaa01afe66cde1126
Mamadou Dieng <diengmd2019@gmail.com>
2025-10-23 15:00:12 +0000

    Pull request

--- src/main/java/sunuguide/controller/IntervalController.java
+++ src/main/java/sunuguide/controller/IntervalController.java
@@ -4,11 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sunuguide.service.IntervalService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/interval")
 public class IntervalController {
 
     private final IntervalService intervalService;
+    Map<String, Object> response = new HashMap<>();
 
     public IntervalController(IntervalService intervalService) {
         this.intervalService = intervalService;
@@ -21,6 +25,9 @@ public class IntervalController {
             @RequestParam double lat2,
             @RequestParam double lon2) {
         double distance = intervalService.calculateDistance(lat1, lon1, lat2, lon2);
+
+        response.put("distance", distance);
+
         return ResponseEntity.ok(distance);
     }
 }
