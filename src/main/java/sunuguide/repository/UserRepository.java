9fabd3e53c4bbfbc4166a2156e7b37eb58933921
Mamadou Dieng <diengmd2019@gmail.com>
2025-10-27 22:26:08 +0000

    Modification Userrepository

--- src/main/java/sunuguide/repository/UserRepository.java
+++ src/main/java/sunuguide/repository/UserRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sunuguide.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Nécessaire pour l'AuthService, mais utilisé indirectement par le UserService.
+     */
+    Optional<User> findByEmail(String email);
 }
